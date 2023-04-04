348e759bea9435a3b94b6f631d2a373f21df6520
JakeJing <350956009@qq.com>
2023-04-04 17:24:08 +0200

    update JS

--- assets/js/blogs/bayes/script.js
+++ assets/js/blogs/bayes/script.js
@@ -32,7 +32,7 @@ simulateButton.addEventListener("click", function () {
       data = sim;
       Input_params["μ"] = mu;
       Input_params["σ"] = sd;
-      window.alert("Simulation OK! Pls start sampling!");
+      window.alert("Simulation OK! Now you can start sampling!");
       // messageDiv.innerHTML = "OK!";
       // setTimeout(function () {
       //   messageDiv.innerHTML = "";
